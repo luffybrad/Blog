0a9b2ffcaf4d089d731a0e0f26628169c3127247
luffy brad <bradleykahoron399@gmail.com>
2022-08-15 20:36:16 +0300

    Unfinished comments and bulma css

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  resources :posts do
+    resources :comments
+  end
+  
+   root "posts#index"
+end
